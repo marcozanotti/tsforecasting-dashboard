34f85f010ed39212e4eb866219969972d12a2f10
Marco Zanotti <zanottimarco17@gmail.com>
2025-09-25 13:19:47 -0300

    fix packages

--- dashboard/R/global-vars.R
+++ dashboard/R/global-vars.R
@@ -3,6 +3,7 @@ globalVariables(
 	names = c(
 		".", "date", "id", "frequency", "value", "where", "n", 
 		".key", ".index", ".value", ".conf_lo", ".conf_hi", ".conf_lvl",
-		".actual", ".prediction", ".model_id", ".model_desc"
+		".actual", ".prediction", ".model_id", ".model_desc", ".residuals",
+		"session"
 	)
 )
